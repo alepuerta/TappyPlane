e44883c0652101f6c0a4053ea1b01d1d76fd4ce3
Alejandro <alejandro@Alejandros-Mac.local>
2015-07-01 23:47:10 +0100

    Add plane class and setup player and world.

---
+++ Tappy Plane/TPPlane.h
@@ -0,0 +1,13 @@
+//
+//  TPPlane.h
+//  Tappy Plane
+//
+//  Created by Alejandro on 1/7/15.
+//  Copyright (c) 2015 Alejandro. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface TPPlane : SKSpriteNode
+
+@end
---
+++ Tappy Plane/TPPlane.m
@@ -0,0 +1,22 @@
+//
+//  TPPlane.m
+//  Tappy Plane
+//
+//  Created by Alejandro on 1/7/15.
+//  Copyright (c) 2015 Alejandro. All rights reserved.
+//
+
+#import "TPPlane.h"
+
+@implementation TPPlane
+
+-(id)init
+{
+    self = [super initWithImageNamed:@"planeBlue1"];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
