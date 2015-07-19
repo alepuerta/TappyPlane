999f74c9ff620c690aadf3f0165bb428aa263136
Alejandro <alejandro@Alejandros-Mac.local>
2015-07-19 23:06:37 +0100

    Create mountains

---
+++ Tappy Plane/TPObstacleLayer.h
@@ -0,0 +1,13 @@
+//
+//  TPObstacleLayer.h
+//  Tappy Plane
+//
+//  Created by Alejandro on 19/7/15.
+//  Copyright (c) 2015 Alejandro. All rights reserved.
+//
+
+#import "TPScrollingNode.h"
+
+@interface TPObstacleLayer : TPScrollingNode
+
+@end
---
+++ Tappy Plane/TPObstacleLayer.m
@@ -0,0 +1,93 @@
+//
+//  TPObstacleLayer.m
+//  Tappy Plane
+//
+//  Created by Alejandro on 19/7/15.
+//  Copyright (c) 2015 Alejandro. All rights reserved.
+//
+
+#import "TPObstacleLayer.h"
+#import "TPConstants.h"
+
+@interface TPObstacleLayer()
+
+@property (nonatomic) CGFloat marker;
+
+@end
+
+static const CGFloat kTPMarkerBuffer = 200.0;
+
+static NSString *const kTPKeyMountainUp = @"MountainUp";
+static NSString *const kTPKeyMountainDown = @"MountainDown";
+
+@implementation TPObstacleLayer
+
+-(void)updateWithTimeElapsed:(NSTimeInterval)timeElapsed
+{
+    [super updateWithTimeElapsed:timeElapsed];
+    
+    if (self.scrolling && self.scene) {
+        // Find marker's location in scene coords.
+        CGPoint markerLocationInScene = [self convertPoint:CGPointMake(self.marker, 0) toNode:self.scene];
+        // When marker comes onto screen, add new obstacles.
+        if (markerLocationInScene.x - (self.scene.size.width * self.scene.anchorPoint.x) < self.scene.size.width + kTPMarkerBuffer) {
+            [self addObstacleSet];
+        }
+        
+    }
+}
+
+-(void)addObstacleSet
+{
+    
+}
+
+-(SKSpriteNode*)createObjectForKey:(NSString*)key
+{
+    SKSpriteNode *object = nil;
+    
+    SKTextureAtlas *atlas = [SKTextureAtlas atlasNamed:@"Graphics"];
+    
+    if (key == kTPKeyMountainUp) {
+        object = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"MountainGrass"]];
+        CGFloat offsetX = object.frame.size.width * object.anchorPoint.x;
+        CGFloat offsetY = object.frame.size.height * object.anchorPoint.y;
+        
+        CGMutablePathRef path = CGPathCreateMutable();
+        
+        CGPathMoveToPoint(path, NULL, 55 - offsetX, 199 - offsetY);
+        CGPathAddLineToPoint(path, NULL, 0 - offsetX, 0 - offsetY);
+        CGPathAddLineToPoint(path, NULL, 90 - offsetX, 0 - offsetY);
+        
+        CGPathCloseSubpath(path);
+        
+        object.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromPath:path];
+        object.physicsBody.categoryBitMask = kTPCategoryGround;
+        
+        [self addChild:object];
+    } else if (key == kTPKeyMountainDown) {
+        object = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"MountainGrassDown"]];
+        CGFloat offsetX = object.frame.size.width * object.anchorPoint.x;
+        CGFloat offsetY = object.frame.size.height * object.anchorPoint.y;
+        
+        CGMutablePathRef path = CGPathCreateMutable();
+        
+        CGPathMoveToPoint(path, NULL, 0 - offsetX, 199 - offsetY);
+        CGPathAddLineToPoint(path, NULL, 55 - offsetX, 0 - offsetY);
+        CGPathAddLineToPoint(path, NULL, 90 - offsetX, 199 - offsetY);
+        
+        CGPathCloseSubpath(path);
+        
+        object.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromPath:path];
+        object.physicsBody.categoryBitMask = kTPCategoryGround;
+        [self addChild:object];
+    }
+    
+    if (object) {
+        object.name = key;
+    }
+    
+    return object;
+}
+
+@end
